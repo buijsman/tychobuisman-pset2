84f8df9e4fb722fe1b787a30fb77970d7cfc004f
Tycho Buisman <Tycho Buisman>
2016-11-07 11:53:35 +0100

    First commit

---
+++ app/src/main/java/tycho/tychobuisman_pset2/activity.java
@@ -0,0 +1,13 @@
+package tycho.tychobuisman_pset2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
